4fb8c259583a9c273d50ac6e2b75027ce15597fc
MacBook <macbook@MacBooks-MacBook-Pro.local>
2019-03-07 07:47:11 +0700

    user login

--- routes/api/users.js
+++ routes/api/users.js
@@ -47,4 +47,30 @@ router.post("/register", (req, res) => {
   });
 });
 
+// @route   GET api/users/register
+// @desc    TESTS post route
+// @access  Public
+
+router.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  //find user by email
+  User.findOne({ email }).then(user => {
+    //check for user
+    if (!user) {
+      return res.status(404).json({ email: "user not found" });
+    }
+
+    // check password
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        res.json({ msg: success });
+      } else {
+        return res.status(400).json({ password: "password incorect" });
+      }
+    });
+  });
+});
+
 module.exports = router;
