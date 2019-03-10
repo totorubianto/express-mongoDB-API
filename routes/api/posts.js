37c7b469d47f6b9f764e39e90ce60833cc23b5de
MacBook <macbook@MacBooks-MacBook-Pro.local>
2019-03-10 12:13:00 +0700

    profile model

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -5,6 +5,11 @@ const router = express.Router();
 // @desc    TESTS post route
 // @access  Public
 
+// Post model
+const Post = require("../../models/Post");
+// Profile model
+const Profile = require("../../models/Profile");
+
 router.get("/test", (req, res) => res.json({ msg: "post Work" }));
 
 module.exports = router;
